37b63c30c1c4d4d1dd44f76fa631d4d0e9ec1644
Adam Toczek <a.a.toczek@gmail.com>
2022-05-07 15:34:08 +0200

    Listy

--- src/main/java/Listy.java
+++ src/main/java/Listy.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Listy {
     public static void main(String[] args) {
@@ -13,6 +15,18 @@ public class Listy {
                 lista.add(i);
             System.out.println(lista.get(i));
         }
+
+
+        // wersja z foreach do mnozenia
+        List<Integer> list2 = new ArrayList<>();
+        for (int i = 0 ; i<=k; i++)
+            list2.add(i);
+
+        for (int v : list2)
+            if (v%2 == 0)
+                list2.set(v, v*2);
+        for (int v : list2)
+            System.out.println(v);
 
 
     }
