22e3d42f08a92cd8de0c43f1462c3cea8e119a7a
Adam Toczek <a.a.toczek@gmail.com>
2022-05-07 16:31:44 +0200

    zadanie domowe - liczby pierwsze

---
+++ src/main/java/ZadanieDomowe1.java
@@ -0,0 +1,16 @@
+/*
+Napisz metode ktora sprawdza czy podana przez uzytkownika liczba jest liczba pierwsza
+liczby wpierwsze to takie ktore dziela sie tylko przez 1 oraz sama siebie:
+2, 3, 5 ,7, 11, 13, .. ..
+
+
+dodatkowo:
+- uzyj osobnej metody do odczytywania danych od uzytkownika
+- dodaj obsluge wyjatkow przy odczytywaniu danych
+- zapetl program - komenda 'exit' powoduje zakonczenie programu
+
+ */
+public class ZadanieDomowe1 {
+
+
+}
