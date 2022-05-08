976532a2e292782fa6642ba5a37c3cc14a743835
Adam Toczek <a.a.toczek@gmail.com>
2022-05-08 15:50:34 +0200

    komentarze

--- src/main/java/warzywniak/Warzywniak.java
+++ src/main/java/warzywniak/Warzywniak.java
@@ -9,13 +9,11 @@ public class Warzywniak {
         Banan banan = new Banan();
         Pomarancz pomarancz = new Pomarancz();
 
-//        System.out.println(japko);
-//        System.out.println(banan);
-//        System.out.println(pomarancz);
-
+        //lista jablek List<Jablko> moze przechowywac tylko elementy typu Jablko
         List<Jablko> listaJablek = new ArrayList<>();
         listaJablek.add(japko);
 
+        //List<Owoc> moze przechowywac jablka, pomarancze i banany poniewaz wszystkie dziedzicza po Owoc
         List<Owoc> koszykOwocow = new ArrayList<>();
         koszykOwocow.add(japko);
         koszykOwocow.add(pomarancz);
