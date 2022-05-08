13f0aa6087a9c832fb04ce9eecc1282fdaa68e6d
Adam Toczek <a.a.toczek@gmail.com>
2022-05-08 14:53:59 +0200

    konstruktory uczniow

--- src/main/java/Uczniowie.java
+++ src/main/java/Uczniowie.java
@@ -1,18 +1,7 @@
 public class Uczniowie {
     public static void main(String[] args) {
-        Student michal = new Student();
-        michal.imie = "Michal";
-        michal.nazwisko = "Piorkowski";
-        michal.srednia = 4.7;
-        michal.wynikMatury = 180;
-        michal.numerIndeksu = "846219";
-
-        Licealista pawel = new Licealista();
-        pawel.imie = "Pawel";
-        pawel.nazwisko = "Kowalski";
-        pawel.srednia = 3.25;
-        pawel.wynikMatury = 143;
-
+        Student michal = new Student("Michal", "Piotrkowski", 4.7, 180, "8465197");
+        Licealista pawel = new Licealista("Pawel", "Kowalski", 3.25, 143);
 
     }
 }
@@ -21,15 +10,35 @@ public class Uczniowie {
 class Uczen {
     String imie;
     String nazwisko;
+
+    public Uczen(String imie, String nazwisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
 }
 class Podstawowkarz extends Uczen {
     double srednia;
+
+    public Podstawowkarz(String imie, String nazwisko, double srednia) {
+        super(imie, nazwisko);
+        this.srednia = srednia;
+    }
 }
 
 class Licealista extends Podstawowkarz {
     int wynikMatury;
+
+    public Licealista(String imie, String nazwisko, double srednia, int wynikMatury) {
+        super(imie, nazwisko, srednia);
+        this.wynikMatury = wynikMatury;
+    }
 }
 
 class Student extends Licealista {
     String numerIndeksu;
+
+    public Student(String imie, String nazwisko, double srednia, int wynikMatury, String numerIndeksu) {
+        super(imie, nazwisko, srednia, wynikMatury);
+        this.numerIndeksu = numerIndeksu;
+    }
 }
