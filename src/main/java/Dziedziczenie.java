ed55e2acd74f8e6b5d9d107d986dd7e3c85e3d05
Adam Toczek <a.a.toczek@gmail.com>
2022-05-08 14:33:57 +0200

    dziedziczenie klas na przykladzie pojazd - osobowka

---
+++ src/main/java/Dziedziczenie.java
@@ -0,0 +1,16 @@
+public class Dziedziczenie {
+}
+
+class Pojazd {
+    String marka;
+    String model;
+    int masa;
+    int rokProdukcji;
+}
+
+class Osobowka extends Pojazd{
+    int iloscMiejsc;
+}
+class Ciezarowka extends Pojazd {
+    int ladownosc;
+}
